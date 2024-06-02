7c747bbeb2d639c350f8f56a3f30a574094d2795
nisslyak <nissopodolyak@gmail.com>
2024-06-02 05:57:32 +0300

    First commit

---
+++ Solution/Solution/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+    public static string pangrams(string s)
+    {
+
+        s = s.ToLower();
+        for (int i = 0; i < 26; i++)
+        {
+
+            char c = Convert.ToChar(i + 97);
+
+            if (!s.Contains(c))
+            {
+                return "not pangram";
+            }
+        }
+        return "pangram";
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        string s = Console.ReadLine();
+
+        string result = Result.pangrams(s);
+    }
+}
